49e1e69ad884c87b512d789998a27cf9a4f9b918
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2024-10-10 21:40:41 -0400

    updated index/markdown

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 // TODO: Include packages needed for this application
 import generateMarkdown from "./utils/generateMarkdown";
-import colors from 'colors';
 import inquirer from 'inquirer';
+import fs from 'fs';
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is your project called?'
+}, {
+    type: 'input',
+    name: 'description',
+    message: 'Describe the project:'
+}, {
+    type: 'input',
+    name: 'installation',
+    message: 'Describe the project:'
+}, {
+    type: 'input',
+    name: 'Usage',
+    message: 'Describe the project:'
+}, {
+    type: 'input',
+    name: 'Contribuiting',
+    message: 'Describe the project:'
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(response) {
+    fs.writeFile('README-Generator.md', response, (err) =>
+        err ? console.error(err) : console.log("data successfully added")
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((response) => {
+            writeToFile(response);
+        }
+        );
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+  if (license === "apache") {
+  return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]"
+  } else if (license === "boost") {
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]"
+  } else if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]"
+  } else {
+    return ""
+  }
+
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "apache") {
+    return "(https://opensource.org/licenses/Apache-2.0)"
+    } else if (license === "boost") {
+      return "(https://www.boost.org/LICENSE_1_0.txt)"
+    } else if (license === "MIT") {
+      return "(https://opensource.org/licenses/MIT)"
+    } else {
+      return ""
+    }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -16,22 +38,28 @@ function generateMarkdown(data) {
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+${data.description}
 
 - What was your motivation?
 - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
 - What problem does it solve?
 - What did you learn?
 
-## Table of Contents (Optional)
+## Installation
 
-If your README is long, add a table of contents to make it easy for users to find what they need.
+${data.installation}
 
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-`;
+## Usage
+
+this is the usage section
+
+## Contributing
+
+this is contributing section
+
+## Tests
+this is a test section
+`
 }
 
 export default generateMarkdown;
